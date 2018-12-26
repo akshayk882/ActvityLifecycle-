4a6af0c94c8cf625fe56156f045dc22e9680382d
akshay <akshayk882.com>
2018-12-26 18:02:13 +0530

    lesson-3

--- app/src/main/java/com/akshaykumbhar/actvitylifecycle/MainActivity.kt
+++ app/src/main/java/com/akshaykumbhar/actvitylifecycle/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.akshaykumbhar.actvitylifecycle
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startbtn.setOnClickListener {
+            var leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
