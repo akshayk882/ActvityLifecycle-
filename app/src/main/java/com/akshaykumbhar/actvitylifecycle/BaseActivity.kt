c566a6db466e6dfa5b41ecfa5132529ffbaa4928
akshay <akshayk882.com>
2018-12-26 21:44:34 +0530

    lesson-6

---
+++ app/src/main/java/com/akshaykumbhar/actvitylifecycle/BaseActivity.kt
@@ -0,0 +1,43 @@
+package com.akshaykumbhar.actvitylifecycle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.sql.ResultSetMetaData
+
+open class BaseActivity : AppCompatActivity() {
+    val TAG = "Lifecycle"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG,"${javaClass.simpleName} onCreate")
+        super.onCreate(savedInstanceState)
+    }
+   override fun onStart()
+   {
+       Log.d(TAG,"${javaClass.simpleName} onStart")
+       super.onStart()
+   }
+
+    override fun onStop() {
+        Log.d(TAG,"${javaClass.simpleName} onStop")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG,"${javaClass.simpleName} onRestart")
+        super.onRestart()
+    }
+    override fun onResume()
+    {
+        Log.d(TAG,"${javaClass.simpleName} onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG,"${javaClass.simpleName} onPause")
+        super.onPause()
+    }
+    override fun onDestroy() {
+        Log.d(TAG,"${javaClass.simpleName} onDestroy")
+        super.onDestroy()
+    }
+}
--- app/src/main/java/com/akshaykumbhar/actvitylifecycle/LeagueActivity.kt
+++ app/src/main/java/com/akshaykumbhar/actvitylifecycle/LeagueActivity.kt
@@ -1,12 +1,20 @@
 package com.akshaykumbhar.actvitylifecycle
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat.startActivity
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+    fun ActivityNextClick(view:View)
+    {
+        val skillIntent = Intent(this, SkillActivity:: class.java)
+        startActivity(skillIntent)
     }
 }
